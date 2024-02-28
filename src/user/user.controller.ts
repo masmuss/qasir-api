d4703acd7f109de447624bc70aeed76c616e4be0
masmuss <ahmusafir.khoirul@gmail.com>
2024-02-29 05:18:54 +0700

    refactor(auth): changing access makes the user from public to admin only
    Signed-off-by: masmuss <ahmusafir.khoirul@gmail.com>

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Response } from '@nestjs/common';
 
-import { UserService } from './user.service';
+import { CanAccessWithRoles } from 'decorators/role.decorator';
+import { Role } from 'enums/role.enum';
+import { AuthService } from 'src/auth/auth.service';
+
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  @CanAccessWithRoles(Role.ADMIN)
+  async create(@Body() createUserDto: CreateUserDto, @Response() response) {
+    const result: boolean = await this.authService.signup(createUserDto);
+
+    if (result) return response.status(201).send({ message: 'User created' });
+    return response.status(400).send();
+  }
 }
