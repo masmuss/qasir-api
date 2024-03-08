bbd853b4b1c37a3a984d3d84ee881411da79c1e1
masmuss <ahmusafir.khoirul@gmail.com>
2024-03-08 19:53:06 +0700

    ci: open homepage access
    Signed-off-by: masmuss <ahmusafir.khoirul@gmail.com>

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import { CanAccessPublic } from './auth/decorators/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @CanAccessPublic()
   getHello(): string {
     return this.appService.getHello();
   }
